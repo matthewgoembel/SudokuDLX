6aa46efb33ec3e5711f9cb1c5dfcf3e5aa1fbdd0
Anthony Clayton <114684904+AnthonyClayton12@users.noreply.github.com>
2025-04-26 16:35:12 -0400

    Update SudokuBackTrack.java

--- SudokuBackTrack.java
+++ SudokuBackTrack.java
@@ -1,1 +1,33 @@
-// Hello World
+// SudokuBackTrack.java
+public class SudokuBackTrack implements SudokuSolver {
+
+    @Override
+    public SudokuBoard solve(SudokuBoard puzzle) {
+        // work on a copy so we don't mutate the original
+        SudokuBoard board = puzzle.copy();
+        solveRec(board);
+        return board;
+    }
+
+    private boolean solveRec(SudokuBoard board) {
+        for (int r = 0; r < SudokuBoard.SIZE; r++) {
+            for (int c = 0; c < SudokuBoard.SIZE; c++) {
+                if (board.get(r, c) == 0) {
+                    for (int val = 1; val <= 9; val++) {
+                        if (board.isValidPlacement(r, c, val)) {
+                            board.set(r, c, val);
+                            if (solveRec(board)) {
+                                return true;
+                            }
+                            board.set(r, c, 0);
+                        }
+                    }
+                    // no valid number -> backtrack
+                    return false;
+                }
+            }
+        }
+        // no empty cell left -> solved
+        return true;
+    }
+}
