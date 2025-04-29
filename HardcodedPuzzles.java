dc2bf14bf08cced456a5ee279e095f3552834d85
Anthony Clayton <114684904+AnthonyClayton12@users.noreply.github.com>
2025-04-29 17:50:46 -0400

    Update HardcodedPuzzles.java

--- HardcodedPuzzles.java
+++ HardcodedPuzzles.java
@@ -2,34 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HardcodedPuzzles {
-    /** Each puzzle is given as an 81-char string, row by row;
-     * use '.' or '0' for blanks.
-     */
+    private static final String[] PUZZLE_NAMES = { "Easy", "Medium", "Hard" };
     private static final String[] PUZZLE_STRINGS = {
-        // Puzzle 1
-        "530070000" +
-        "600195000" +
-        "098000060" +
-        "800060003" +
-        "400803001" +
-        "700020006" +
-        "060000280" +
-        "000419005" +
-        "000080079",
-
-        // Puzzle 2
-        "100007090" +
-        "030020008" +
-        "009600500" +
-        "005300900" +
-        "010080002" +
-        "600004000" +
-        "059000073" +
-        "002010080" +
-        "070000100"
+        // Easy
+        "530070000600195000098000060800060003400803001700020006060000280000419005000080079",
+        // Medium
+        "003020600900305001001806400008102900700000008006708200002609500800203009005010300",
+        // Hard
+        "000000907000420180000705026100904000050000040000507009920108000034059000507000000"
     };
 
-    /** Returns a list of SudokuBoard instances for each hard-coded puzzle. */
+    /** Returns a list of puzzle names in the same order as the puzzles. */
+    public static List<String> getNames() {
+        List<String> names = new ArrayList<>();
+        for (String n : PUZZLE_NAMES) names.add(n);
+        return names;
+    }
+
+    /** Returns a list of SudokuBoard instances for Easy, Medium, and Hard. */
     public static List<SudokuBoard> getPuzzles() {
         List<SudokuBoard> list = new ArrayList<>();
         for (String s : PUZZLE_STRINGS) {
