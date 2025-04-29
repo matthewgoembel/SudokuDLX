4c95fa68b161c3d74cf8cca714fa0a060ff91907
Anthony Clayton <114684904+AnthonyClayton12@users.noreply.github.com>
2025-04-29 18:49:28 -0400

    Update HardcodedPuzzles.java

--- HardcodedPuzzles.java
+++ HardcodedPuzzles.java
@@ -2,50 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HardcodedPuzzles {
-    private static final String[] PUZZLE_NAMES = { "Easy", "Medium", "Hard" };
-    private static final String[] PUZZLE_STRINGS = {
-      // Easy (~41 clues, high)
-      "530070000"
-      + "600195000"
-      + "098342560"
-      + "800060003"
-      + "400803001"
-      + "700020046"
-      + "061530280"
-      + "000419005"
-      + "090080079",
-        
-        // medium (~32 clues, moderate)
-        "003020600"
-      + "900305001"
-      + "001806400"
-      + "008102900"
-      + "700000008"
-      + "006708200"
-      + "002609500"
-      + "800203009"
-      + "005010300",
-
-        // hard (~17 clues, low) 
-        "000000002"
-      + "000006000"
-      + "001000090"
-      + "000100000"
-      + "000080000"
-      + "000003000"
-      + "040000700"
-      + "000200000"
-      + "300000000"
+    private static final String[] PUZZLE_NAMES = {
+        "Easy (39 clues)",
+        "Medium (32 clues)",
+        "Hard (17 clues)"
     };
 
-    /** Returns a list of puzzle names in the same order as the puzzles. */
+    private static final String[] PUZZLE_STRINGS = {
+        // Easy (39 clues)
+        "530070000600195000098342560800060003400803001700020046061530280000419005090080079",
+        // Medium (32 clues, Project Euler #96)
+        "003020600900305001001806400008102900700000008006708200002609500800203009005010300",
+        // Hard (17 clues, minimal)
+        "000000002000006000001000090000100000000800000000030000040000700000200000300000000"
+    };
+
     public static List<String> getNames() {
         List<String> names = new ArrayList<>();
         for (String n : PUZZLE_NAMES) names.add(n);
         return names;
     }
 
-    /** Returns a list of SudokuBoard instances for Easy, Medium, and Hard. */
     public static List<SudokuBoard> getPuzzles() {
         List<SudokuBoard> list = new ArrayList<>();
         for (String s : PUZZLE_STRINGS) {
